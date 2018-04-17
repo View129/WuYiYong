f3265aad71a6bddc9eb16385497ffd8395a36945
view <1336646875@qq.com>
2018-04-17 15:08:21 +0800

    log4j

---
+++ springmvc/src/main/java/com/wyy/log4j/app.java
@@ -0,0 +1,32 @@
+package com.issc.second;
+
+import org.apache.log4j.Logger;
+
+public class app {
+    static Logger logger = Logger.getLogger(app.class);
+    public static void main(String[] args) {
+        logger.debug("debugdebugdebugdebugdebugdebugdebug");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+        logger.error("errorerrorerrorerrorerrorerrorerrorerror");
+    }
+}
